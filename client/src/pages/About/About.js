28a9a8cf69058dac26fa901df4e9ab065d9686b1
Abhishek kumar <116268169+abhishekms7@users.noreply.github.com>
2025-04-28 23:38:40 +0530

    Update About.js

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -20,7 +20,7 @@ const About = () => {
         </h1>
         <Link to="/shop">
           <button className="w-52 h-10 bg-primeColor text-white hover:bg-black duration-300">
-            mohit
+            abhishek 
           </button>
         </Link>
       </div>
